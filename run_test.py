8e0e8cbe0682d8987fe3fd99107fa21a9727849e
Evansika959 <evansjiang0959@gmail.com>
2024-10-03 00:38:07 -0400

    copy files from google transformer

--- run_test.py
+++ run_test.py
@@ -1,6 +1,6 @@
 # pip install accelerate
 from transformers import AutoTokenizer
-from switch_transformer import SwitchTransformersForConditionalGeneration
+from model_src.switch_transformer import SwitchTransformersForConditionalGeneration
 
 tokenizer = AutoTokenizer.from_pretrained("google/switch-base-64")
 model = SwitchTransformersForConditionalGeneration.from_pretrained("google/switch-base-64", device_map="auto")
