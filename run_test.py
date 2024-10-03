d68a0829a7111f2730a9eac3539375933d304120
Evansika959 <evansjiang0959@gmail.com>
2024-10-03 00:03:47 -0400

    add test file

---
+++ run_test.py
@@ -0,0 +1,13 @@
+# pip install accelerate
+from transformers import AutoTokenizer
+from switch_transformers import SwitchTransformersForConditionalGeneration
+
+tokenizer = AutoTokenizer.from_pretrained("google/switch-base-64")
+model = SwitchTransformersForConditionalGeneration.from_pretrained("google/switch-base-64", device_map="auto")
+
+input_text = "A <extra_id_0> walks into a bar a orders a <extra_id_1> with <extra_id_2> pinch of <extra_id_3>."
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to(0)
+
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0]))
+
