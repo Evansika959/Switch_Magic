a6f3a52108c80e39fe28665f5646399abed135d9
Evansika959 <evansjiang0959@gmail.com>
2024-10-09 14:22:19 -0400

    add lrp test

--- get_de_en_LRP.py
+++ get_de_en_LRP.py
@@ -6,7 +6,33 @@ from transformers_cp.src.transformers.models.switch_transformers import SwitchTr
 from captum.attr import LayerLRP
 from captum.attr._utils.lrp_rules import EpsilonRule, IdentityRule
 import matplotlib.pyplot as plt
+import torch.nn as nn
 import numpy as np
+
+#dummy class
+class SwitchTransformersLayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-6):
+        """
+        Construct a layernorm module in the SwitchTransformers style. No bias and no subtraction of mean.
+        """
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, hidden_states):
+        # SwitchTransformers uses a layer_norm which only scales and doesn't shift, which is also known as Root Mean
+        # Square Layer Normalization https://arxiv.org/abs/1910.07467 thus varience is calculated
+        # w/o mean and there is no bias. Additionally we want to make sure that the accumulation for
+        # half-precision inputs is done in fp32
+
+        variance = hidden_states.to(torch.float32).pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+
+        # convert into half-precision if necessary
+        if self.weight.dtype in [torch.float16, torch.bfloat16]:
+            hidden_states = hidden_states.to(self.weight.dtype)
+
+        return self.weight * hidden_states
 
 # Step 1: Load the Model and Tokenizer
 model_name = 'google/switch-base-8'  # Replace with 'google/switch_transformer-base-8' when available
