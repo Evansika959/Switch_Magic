5be5f4d495f8edc2818fa1a1ded5e6e47292dbd1
Evansika959 <evansjiang0959@gmail.com>
2024-10-10 12:54:33 -0400

    update LRP

--- captum_cp/attr/_core/layer/layer_lrp.py
+++ captum_cp/attr/_core/layer/layer_lrp.py
@@ -212,7 +212,6 @@ class LayerLRP(LRP, LayerAttribution):
         self._original_state_dict = self.model.state_dict()
         self.layers = []
         self._get_layers(self.model)
-        print(self.layers)
         self._check_and_attach_rules()
         self.attribute_to_layer_input = attribute_to_layer_input
         self.backward_handles = []
@@ -277,6 +276,7 @@ class LayerLRP(LRP, LayerAttribution):
         if isinstance(self.layer, list):
             relevances = []
             for layer in self.layer:
+                print("At layer: ", layer)
                 relevances.append(self._get_single_output_relevance(layer, output))
             return relevances
         else:
