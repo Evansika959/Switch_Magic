b1566336f4fa5c9b09b2b31fa057586a79d89bbc
Evansika959 <evansjiang0959@gmail.com>
2024-10-11 01:58:49 -0400

    compute head confidence

--- plot_heat_map.py
+++ plot_heat_map.py
@@ -76,18 +76,18 @@ def plot_confidence_map(data, filename="confidence_map", title="Heatmap of Confi
     plt.figure(figsize=(15, 12))  # Set the figure size
     plt.imshow(data, cmap="Blues", interpolation="none")  # Use imshow to plot the heatmap
     cbar = plt.colorbar()
-    cbar.set_label("Average Confidence", fontsize=14)
-    cbar.ax.tick_params(labelsize=12)
-    plt.title(title, fontsize=16)  # Set the title of the heatmap
-    plt.xlabel("Attention Heads", fontsize=14)
-    plt.ylabel("Layers", fontsize=14)
+    cbar.set_label("Average Confidence", fontsize=20)
+    cbar.ax.tick_params(labelsize=18)
+    plt.title(title, fontsize=24)  # Set the title of the heatmap
+    plt.xlabel("Attention Heads", fontsize=22)
+    plt.ylabel("Layers", fontsize=22)
     # Annotate the heatmap with the confidence values
     # for i in range(len(data)):
     #     for j in range(len(data[0])):
     #         plt.text(j, i, f'{data[i][j]:.2f}', ha='center', va='center', color='black')
 
-    plt.xticks(ticks=range(len(data[0])), labels=[f'Head {i}' for i in range(len(data[0]))], fontsize=12)  # Set x-axis ticks
-    plt.yticks(ticks=range(len(data)), labels=[f'Layer {i}' for i in range(len(data))], fontsize=12)  # Set y-axis ticks
+    plt.xticks(ticks=range(len(data[0])), labels=[f'Head {i}' for i in range(len(data[0]))], fontsize=18)  # Set x-axis ticks
+    plt.yticks(ticks=range(len(data)), labels=[f'Layer {i}' for i in range(len(data))], fontsize=18)  # Set y-axis ticks
     plt.tight_layout()
     plt.savefig(f"plots/{filename}.png")  # Save the heatmap as a PNG file
 
