2b1aba11533461a14505a7f43bcf64d5e56b3717
Evansika959 <evansjiang0959@gmail.com>
2024-10-17 16:18:17 -0400

    add de2en training

--- inference_test.py
+++ inference_test.py
@@ -13,7 +13,7 @@ model = SwitchTransformersForConditionalGeneration.from_pretrained(
     "google/switch-base-8",
     device_map="auto"  # Automatically distribute the model across available devices
 )
-# model.load_state_dict(torch.load('./checkpoints_switch_forLRP/best_switch_transformer.pth'))
+model.load_state_dict(torch.load('./checkpoints_switch_de2en/best_switch_transformer.pth'))
 
 # Set the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -23,42 +23,42 @@ model.to(device)
 dataset = load_dataset("wmt16", "de-en")
 
 # Randomize the test iteration
-# import random
-# random.seed(42)
-# test_num = 5
+import random
+random.seed(42)
+test_num = 5
 
-# for i in range(test_num):
-#     # randomly select 1 test case
-#     idx = random.randint(0, len(dataset["test"]))
+for i in range(test_num):
+    # randomly select 1 test case
+    idx = random.randint(0, len(dataset["test"]))
 
-#     # Select a test case from the dataset
-#     test_case = dataset["test"][idx]
-#     print("Original English Sentence:")
-#     print(test_case["translation"]["en"])
+    # Select a test case from the dataset
+    test_case = dataset["test"][idx]
+    print("Original Sentence:")
+    print(test_case["translation"]["de"])
 
-#     # Tokenize the input
-#     input_text = test_case["translation"]["en"]
-#     inputs = tokenizer(input_text, return_tensors="pt", max_length=128, truncation=True).to(device)
+    # Tokenize the input
+    input_text = test_case["translation"]["de"]
+    inputs = tokenizer(input_text, return_tensors="pt", max_length=128, truncation=True).to(device)
 
-#     # Generate translation
-#     model.eval()
-#     with torch.no_grad():
-#         outputs = model.generate(**inputs, max_length=128, num_beams=4, early_stopping=True)
+    # Generate translation
+    model.eval()
+    with torch.no_grad():
+        outputs = model.generate(**inputs, max_length=128, num_beams=4, early_stopping=True)
 
-#     # Decode the generated tokens
-#     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    # Decode the generated tokens
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-#     # Print the generated translation
-#     print("Generated German Translation:")
-#     print(generated_text)
+    # Print the generated translation
+    print("Generated German Translation:")
+    print(generated_text)
 
-#     print("Reference German Translation:")
-#     print(test_case["translation"]["de"])
-#     print(f"Case {i+1} of {test_num} Finished")
-#     print("\n")
+    print("Reference German Translation:")
+    print(test_case["translation"]["en"])
+    print(f"Case {i+1} of {test_num} Finished")
+    print("\n")
 
 # Tokenize the input
-input_text = "Translate to English: " + "Was ist das"
+input_text = "Was ist das"
 inputs = tokenizer(input_text, return_tensors="pt", max_length=128, truncation=True).to(device)
 
 print("Input:")
