48ae67029348a875383597a984ec6758f231c5a7
Evansika959 <evansjiang0959@gmail.com>
2024-10-22 16:15:12 -0400

    add spacy

---
+++ pos_test.py
@@ -0,0 +1,113 @@
+import torch
+from transformers_cp.src.transformers.models.switch_transformers import SwitchTransformersForConditionalGeneration, SwitchTransformersSparseMLP
+from transformers import AutoTokenizer
+from datasets import load_dataset
+from torchsummary import summary
+import re
+from plot_heat_map import plot_heat_map
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+# Load the tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("google/switch-base-8")
+model = SwitchTransformersForConditionalGeneration.from_pretrained(
+    "google/switch-base-8",
+    device_map="auto"  # Automatically distribute the model across available devices
+)
+model.load_state_dict(torch.load('./checkpoints_switch_de2en/best_switch_transformer.pth'))
+
+# Set the device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# Load the WMT dataset
+dataset = load_dataset("wmt16", "de-en")
+
+# Randomize the test iteration
+import random
+random.seed(42)
+test_num = 1
+
+for i in range(test_num):
+    # randomly select 1 test case
+    idx = random.randint(0, len(dataset["test"]))
+
+    # Select a test case from the dataset
+    test_case = dataset["test"][idx]
+    # print("Original Sentence:")
+    # print(test_case["translation"]["de"])
+
+    # Tokenize the input
+    input_text = test_case["translation"]["de"]
+    inputs = tokenizer(input_text, return_tensors="pt", max_length=128, truncation=True).to(device)
+
+    # Generate translation
+    model.eval()
+    with torch.no_grad():
+        outputs = model.generate(**inputs, max_length=128, num_beams=4, early_stopping=True)
+
+    # Decode the generated tokens
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    # Print the generated translation
+    # print("Generated German Translation:")
+    # print(generated_text)
+
+    # print("Reference German Translation:")
+    # print(test_case["translation"]["en"])
+    # print(f"Case {i+1} of {test_num} Finished")
+    # print("\n")
+
+# Tokenize the input
+input_text = "Was ist das"
+inputs = tokenizer(input_text, return_tensors="pt", max_length=128, truncation=True).to(device)
+
+print("Input:")
+print(input_text)
+
+doc = nlp(input_text)
+
+# 打印每个词的词性
+for token in doc:
+    print(f"{token.text}: {token.pos_}")
+
+# Generate translation
+model.eval()
+with torch.no_grad():
+    outputs = model.generate(**inputs, max_length=128, num_beams=4, early_stopping=True)
+
+# Decode the generated tokens
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+# Print the generated translation
+print("Generated Translation:")
+print(generated_text)
+
+# Regex pattern to match all strings starting with "encoder" and ending with ".mlp"
+pattern = r'^encoder\..*\.mlp$'
+pattern2 = r'^decoder\..*\.mlp$'
+
+
+encoder_router_history = {}
+decoder_router_history = {}
+
+for name, module in model.named_modules():
+    if re.match(pattern, name) and isinstance(module, SwitchTransformersSparseMLP):
+        # print(name)
+        # print(module.router_history)
+        encoder_router_history[re.search(r'encoder\.block\.\d+', name).group()] = torch.cat(module.router_history).flatten()
+        # print("\n")
+    if re.match(pattern2, name) and isinstance(module, SwitchTransformersSparseMLP):
+        # print(name)
+        # print(module.router_history)
+        decoder_router_history[re.search(r'decoder\.block\.\d+', name).group()] = torch.cat(module.router_history).flatten()
+        # print("\n")
+
+
+# plot_heat_map(encoder_router_history, filename="encoder_router_history_de2en", title="Router History of Encoder Blocks")
+# plot_heat_map(decoder_router_history, filename="decoder_router_history_de2en", title="Router History of Decoder Blocks")
+
+#calculate LRP
+
